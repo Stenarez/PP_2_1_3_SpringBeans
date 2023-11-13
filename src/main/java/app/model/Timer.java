4a67feae57d21fe5021641871b21afc4be2d6d58
Stenarez <stena@bk.ru>
2023-11-13 10:51:27 +0300

    r

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,14 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
-
+    private final Long nanoTime = System.nanoTime();
+@Scope("singletone")
     public Long getTime() {
         return nanoTime;
     }
